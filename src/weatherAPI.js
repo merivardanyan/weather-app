78e9587467ce82ad445753e29bc95b4f5acc11bd
Meri Vardanyan <ms.merivardanyan@gmail.com>
2025-12-30 08:36:00 -0800

    remove leftover node-fetch, and return data  instead of console checking

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,17 +1,14 @@
-// const fetch = require('node-fetch');
-
 const getweatherAPI = async (city) => {
     const location = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
     const locationData = await location.json();
     // console.log(locationData);
-    const latitude = locationData.results[0].latitude;
-    const longitude = locationData.results[0].longitude;
+    if (locationData.results.length === 0 || !locationData.results) {
+        throw new Error('City not found');
+    }
+    const {latitude,longitude} = locationData.results[0];
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
         const data = await response.json();
-        console.log(data);
-    
-    // return data;
+        // console.log(data);
+    return data.current_weather;
 }
-
-getweatherAPI('london');
-// module.exports = weatherAPI;
+module.exports = { getweatherAPI };
